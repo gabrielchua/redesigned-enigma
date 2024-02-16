7ef392695cd810dd16704faec9b854b6f589f41f
Gabriel CHUA <gabriel_chua@tech.gov.sg>
2024-02-16 23:05:45 +0800

    update files

--- skills_extraction.py
+++ skills_extraction.py
@@ -6,9 +6,9 @@ import os
 import instructor
 from openai import OpenAI
 from pydantic import BaseModel, Field
+from ssg_sea.extract_skills import extract_skills
 
 azure_client = instructor.patch(OpenAI(api_key=os.environ["OPENAI_API_KEY"]))
-
 
 class IdentifiedSkills(BaseModel):
     """
@@ -19,7 +19,6 @@ class IdentifiedSkills(BaseModel):
     tools: list[str] = Field(
         min_items=0, max_items=30, description="Tools/Software/Frameworks."
     )
-
 
 def extract_free_text_skills(gpt_model, text):
     """
@@ -42,3 +41,35 @@ def extract_free_text_skills(gpt_model, text):
     extracted_skills = response.skills + response.tools
 
     return extracted_skills
+
+def ssg_skills_extraction(text: str):
+    """
+    Extract skills using SSG-SEA
+    """
+    ssg_sea_output = extract_skills(text)
+
+    skill_extract_list = []
+
+    for skill in ssg_sea_output['extractions'].values():
+        skill_extract = {}
+        skill_extract['title'] = skill['skill_title']
+        skill_extract['type'] = skill['skill_type']
+        skill_extract_list.append(skill_extract)
+
+    hard_skills = []
+    soft_skills = []
+    tools = []
+
+    for skill in skill_extract_list:
+        if skill['type'] == "TSC":
+            hard_skills.append(skill['title'])
+        elif skill['type'] == "CCS":
+            soft_skills.append(skill['title'])
+        else:
+            tools.append(skill['title'])
+
+    ssg_sea_output_flatted = {"hard_skills": hard_skills,
+                                "soft_skills": soft_skills,
+                                "tools": tools}
+
+    return ssg_sea_output_flatted
